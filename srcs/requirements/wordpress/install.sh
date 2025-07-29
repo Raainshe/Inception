70758cdf8a1c50a745a4945a8338ec16dd8b53fc
Raain <ryanbwgt@gmail.com>
2025-07-29 15:20:23 +0200

    Dockerfile for wordpress complete. Need to finish the install.sh script to call the right env variables for the table

---
+++ srcs/requirements/wordpress/install.sh
@@ -0,0 +1,16 @@
+cd /var/www/html
+
+if [! -f wp-cli.phar]; then
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+fi
+
+if [! -f wp-config.php]; then
+    ./wp-cli.phar core download --allow-root
+    ./wp-cli.phar config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
+    ./wp-cli.phar core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root
+    ./wp-cli.phar user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root
+
+fi
+
+php-fpm7.4 -F
