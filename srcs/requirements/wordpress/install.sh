785240a1f48a73ed9c9e9a51a23fc78f24ff884a
Raain <ryanbwgt@gmail.com>
2025-07-30 14:58:16 +0200

    completed docker-compose file

--- srcs/requirements/wordpress/install.sh
+++ srcs/requirements/wordpress/install.sh
@@ -1,28 +1,29 @@
+#!/bin/bash
 cd /var/www/html
 
-if [! -f wp-cli.phar]; then
+if [ ! -f wp-cli.phar ]; then
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
 fi
 
-if [! -f wp-config.php]; then
+if [ ! -f wp-config.php ]; then
     ./wp-cli.phar core download --allow-root
     ./wp-cli.phar config create \
-        --dbname=$MYSQL_DATABASE \
-        --dbuser=$MYSQL_USER \
-        --dbpass=$MYSQL_PASSWORD \
-        --dbhost=$MYSQL_HOST \
+        --dbname="$MYSQL_DATABASE" \
+        --dbuser="$MYSQL_USER" \
+        --dbpass="$MYSQL_PASSWORD" \
+        --dbhost="$MYSQL_HOST" \
         --allow-root
     ./wp-cli.phar core install \
-        --url=$DOMAIN_NAME \
-        --title=$WP_TITLE \
-        --admin_user=$WP_ADMIN \
-        --admin_password=$WP_ADMIN_PASS \
-        --admin_email=$WP_ADMIN_EMAIL \
+        --url="$DOMAIN_NAME" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN" \
+        --admin_password="$WP_ADMIN_PASS" \
+        --admin_email="$WP_ADMIN_EMAIL" \
         --allow-root
-    ./wp-cli.phar user create $WP_USER $WP_USER_EMAIL \
+    ./wp-cli.phar user create "$WP_USER" "$WP_USER_EMAIL" \
         --role=author \
-        --user_pass=$WP_USER_PASS \
+        --user_pass="$WP_USER_PASS" \
         --allow-root
 
 fi
